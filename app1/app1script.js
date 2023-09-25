0e0d5fd33ab773d20da095d210ce62ba783df329
ip.osinga <ip.osinga@alfa-college.nl>
2023-09-25 13:03:30 +0200

    v2023-09-25

--- app1/app1script.js
+++ app1/app1script.js
@@ -2,12 +2,22 @@
 let teller = 1
 let uitkomst = 0
 let tekst = ""
-while(teller < 51)
+
+
+
+function showTafel()
 {
-    uitkomst = teller * 113
-    tekst += teller + " x 113 =" + uitkomst + "<br>"
-    //hoog de teller met 1 op
-    teller++
+    //variabele die de waarde heeft van het input veld 'inputTafelvan'
+    let tafelvan = document.getElementById("inputTafelvan").value
+    //variabele die de waarde heeft van het input veld 'inputTafeltot'
+    let tafeltot = document.getElementById("inputTafeltot").value
+    while(teller <= tafeltot)
+    {
+        uitkomst = teller * tafelvan
+        tekst += teller + " x " + tafelvan + " = " + uitkomst + "<br>"
+        //hoog de teller met 1 op
+        teller++
+    }
+    //de inhoud van tekst in mijn html pushen
+    document.getElementById("tafel").innerHTML = tekst
 }
-//de inhoud van tekst in mijn html pushen
-document.getElementById("tafel").innerHTML = tekst
